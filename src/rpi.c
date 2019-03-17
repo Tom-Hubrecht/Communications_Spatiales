557837a7c2978efe6a1d1dfeaa4a4c46d280ad35
Tom Hubrecht <tom@hubrecht.fr>
2019-03-17 17:24:36 +0100

    update output

--- src/rpi.c
+++ src/rpi.c
@@ -1,17 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+
+
 #include <wiringPi.h>
 #include "bit_array.h"
 #include "bar.h"
 
+#include "rpi.h"
+
 void send(bar *message)
 {
+    char m;
+    char r;
+
     wiringPiSetup();
     pinMode(0, OUTPUT);
+    pinMode(29, INPUT);
 
     size_t n = barlen(message);
     for(size_t i = 0; i < n; i++)
     {
-        digitalWrite(0, barget(message, i));
-        delay(100);
+        m = barget(message, i);
+        digitalWrite(0, m);
+        r = digitalRead(29);
+//        printf("%d -- %d\n", m, r);
+//        delay(100);
     }
 
 }
---
+++ src/rpi.h
@@ -0,0 +1,10 @@
+#include <wiringPi.h>
+#include "bit_array.h"
+#include "bar.h"
+
+#ifndef RPI_H
+#define RPI_H
+
+void send(bar *message);
+
+#endif
